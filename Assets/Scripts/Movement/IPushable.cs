9357cf174faf0d059f2097b867827074f523cf58
Max Himmel <himmelmax@gmail.com>
2023-07-18 14:05:54 -0700

    imported pushable interface
    + maybe the ship can implement this to interact w/a gun module

---
+++ Assets/Scripts/Movement/IPushable.cs
@@ -0,0 +1,12 @@
+﻿using ShootBalls.Utility;
+using UnityEngine;
+
+namespace ShootBalls.Gameplay
+{
+	public interface IPushable
+	{
+		IOrientation Orientation { get; }
+
+		void Push( Vector2 velocity );
+	}
+}
