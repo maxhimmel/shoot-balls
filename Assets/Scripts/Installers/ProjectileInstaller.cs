7b7ab77ff27478a11ba840d267dd65d50f55f2b6
Max Himmel <himmelmax@gmail.com>
2023-07-18 14:04:49 -0700

    created basic projectile
    + w/prefab
    + w/installer

---
+++ Assets/Scripts/Installers/ProjectileInstaller.cs
@@ -0,0 +1,27 @@
+﻿using ShootBalls.Gameplay;
+using UnityEngine;
+using Zenject;
+
+namespace ShootBalls.Installers
+{
+	public class ProjectileInstaller : Installer<ProjectileInstaller>
+	{
+		private readonly Projectile.Settings _settings;
+
+		public ProjectileInstaller( Projectile.Settings settings )
+		{
+			_settings = settings;
+		}
+
+		public override void InstallBindings()
+		{
+			Container.Bind<Projectile>()
+				.AsSingle()
+				.WithArguments( _settings );
+
+			Container.Bind<Rigidbody2D>()
+				.FromComponentOnRoot()
+				.AsSingle();
+		}
+	}
+}
---
+++ Assets/Scripts/Projectiles/Projectile.cs
@@ -0,0 +1,64 @@
+using System.Threading;
+using Cysharp.Threading.Tasks;
+using ShootBalls.Utility;
+using UnityEngine;
+using Zenject;
+
+namespace ShootBalls.Gameplay
+{
+	public class Projectile : IOrientation
+	{
+		public event System.Action<Projectile> Destroyed;
+
+		public Vector2 Position {
+			get => _body.position;
+			set => _body.position = value;
+		}
+		public Quaternion Rotation {
+			get => _body.rotation.To2DRotation();
+			set => _body.SetRotation( value );
+		}
+		public Transform Parent {
+			get => _settings.Owner;
+			set => _settings.Owner = value;
+		}
+
+		private readonly Rigidbody2D _body;
+		private readonly Settings _settings;
+		private readonly CancellationToken _onDestroyedCancelToken;
+
+		public Projectile( Rigidbody2D body,
+			Settings settings )
+		{
+			_body = body;
+			_settings = settings;
+
+			_onDestroyedCancelToken = body.GetCancellationTokenOnDestroy();
+		}
+
+		public void Launch( Vector2 direction )
+		{
+			_body.AddForce( direction, ForceMode2D.Impulse );
+
+			HandleLifetime().Forget();
+		}
+
+		private async UniTaskVoid HandleLifetime()
+		{
+			await TaskHelpers.DelaySeconds( _settings.Lifetime, _onDestroyedCancelToken );
+
+			Destroyed?.Invoke( this );
+			GameObject.Destroy( _body.gameObject );
+		}
+
+		public class Factory : PlaceholderFactory<Settings, Projectile> { }
+
+		[System.Serializable]
+		public class Settings
+		{
+			public Transform Owner { get; set; }
+
+			public float Lifetime;
+		}
+	}
+}
