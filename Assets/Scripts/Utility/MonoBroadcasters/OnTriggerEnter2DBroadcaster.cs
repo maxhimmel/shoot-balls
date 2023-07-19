345e98cc0825c56771a78a184381f7323c7329ad
Max Himmel <himmelmax@gmail.com>
2023-07-19 15:30:07 -0700

    more mono message utilities for trigger enter/exit

---
+++ Assets/Scripts/Utility/MonoBroadcasters/OnTriggerEnter2DBroadcaster.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using Zenject;
+
+namespace ShootBalls.Utility
+{
+	public class OnTriggerEnter2DBroadcaster : MonoBehaviour
+	{
+		public event System.Action<Collider2D> Entered;
+
+		private void OnTriggerEnter2D( Collider2D collision )
+		{
+			Entered?.Invoke( collision );
+		}
+
+		public class Factory : PlaceholderFactory<OnTriggerEnter2DBroadcaster> { }
+	}
+}
---
+++ Assets/Scripts/Utility/MonoBroadcasters/OnTriggerExit2DBroadcaster.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using Zenject;
+
+namespace ShootBalls.Utility
+{
+	public class OnTriggerExit2DBroadcaster : MonoBehaviour
+	{
+		public event System.Action<Collider2D> Exited;
+
+		private void OnTriggerExit2D( Collider2D collision )
+		{
+			Exited?.Invoke( collision );
+		}
+
+		public class Factory : PlaceholderFactory<OnTriggerEnter2DBroadcaster> { }
+	}
+}
