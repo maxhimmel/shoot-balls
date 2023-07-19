40abe362c728aea78f7c19e16ab4e9ead0fba2de
Max Himmel <himmelmax@gmail.com>
2023-07-18 18:07:46 -0700

    imported task runner helper

---
+++ Assets/Scripts/Utility/TaskRunner.cs
@@ -0,0 +1,53 @@
+﻿using System.Threading;
+using Cysharp.Threading.Tasks;
+
+namespace ShootBalls.Utility
+{
+	public class TaskRunner
+	{
+		public bool IsRunning { get; private set; }
+		public CancellationToken CancelToken => _cancelSource.Token;
+
+		private CancellationTokenSource _cancelSource;
+		private CancellationToken? _outerCancelToken;
+
+		public TaskRunner()
+		{
+			_cancelSource = new CancellationTokenSource();//AppHelper.CreateLinkedCTS();
+		}
+		public TaskRunner( CancellationToken cancelToken )
+		{
+			_outerCancelToken = cancelToken;
+			_cancelSource = CancellationTokenSource.CreateLinkedTokenSource( cancelToken );//AppHelper.CreateLinkedCTS( cancelToken );
+		}
+
+		public async UniTask Run( System.Func<UniTask> task )
+		{
+			TryCancel();
+
+			IsRunning = true;
+
+			await task
+				.Invoke()
+				.Cancellable( _cancelSource.Token );
+
+			IsRunning = false;
+		}
+
+		public bool TryCancel()
+		{
+			if ( !IsRunning )
+			{
+				return false;
+			}
+
+			_cancelSource.Cancel();
+			_cancelSource.Dispose();
+			_cancelSource = _outerCancelToken.HasValue && _outerCancelToken.Value.CanBeCanceled
+				? CancellationTokenSource.CreateLinkedTokenSource( _outerCancelToken.Value )//AppHelper.CreateLinkedCTS( _outerCancelToken.Value )
+				: new CancellationTokenSource();//AppHelper.CreateLinkedCTS();
+
+			return true;
+		}
+	}
+}
