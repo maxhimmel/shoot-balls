974f3e8789c8d0a747b9c69dce2190026de45ade
Max Himmel <himmelmax@gmail.com>
2023-07-19 15:35:33 -0700

    player explicitly controls motor and implements ipawn

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -6,19 +6,26 @@ using Zenject;
 
 namespace ShootBalls.Gameplay.Player
 {
-	public class PlayerController : ITickable
+	public class PlayerController : IPawn,
+		ITickable,
+		IFixedTickable
 	{
+		public Rigidbody2D Body => _body;
+
 		private readonly Rewired.Player _input;
 		private readonly CharacterMotor _motor;
 		private readonly Gun _gun;
+		private readonly Rigidbody2D _body;
 
 		public PlayerController( Rewired.Player input,
 			CharacterMotor motor,
-			Gun gun )
+			Gun gun,
+			Rigidbody2D body )
 		{
 			_input = input;
 			_motor = motor;
 			_gun = gun;
+			_body = body;
 		}
 
 		public void Tick()
@@ -45,5 +52,12 @@ namespace ShootBalls.Gameplay.Player
 				_gun.StopFiring();
 			}
 		}
+
+		public void FixedTick()
+		{
+			_motor.FixedTick();
+		}
+
+		public class Factory : PlaceholderFactory<PlayerController> { }
 	}
 }
