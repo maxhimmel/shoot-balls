c9d82d3bf114513242079224659b1ecc196033ed
Max Himmel <himmelmax@gmail.com>
2023-07-20 10:42:14 -0700

    creating damageable interface w/data

---
+++ Assets/Scripts/Damageable/IDamageData.cs
@@ -0,0 +1,28 @@
+﻿using ShootBalls.Utility;
+using UnityEngine;
+
+namespace ShootBalls.Gameplay.Pawn
+{
+	public interface IDamageData
+	{
+		System.Type HandlerType { get; }
+
+		IPawn Instigator { get; set; }
+		IPawn Causer { get; set; }
+
+		Vector2 HitPosition { get; set; }
+		Vector2 HitNormal { get; set; }
+	}
+
+	public abstract class DamageData<THandler> : IDamageData
+		where THandler : IDamageHandler
+	{
+		public virtual System.Type HandlerType => typeof( THandler );
+
+		public IPawn Instigator { get; set; }
+		public IPawn Causer { get; set; }
+
+		public Vector2 HitPosition { get; set; }
+		public Vector2 HitNormal { get; set; }
+	}
+}
---
+++ Assets/Scripts/Damageable/IDamageable.cs
@@ -0,0 +1,8 @@
+﻿namespace ShootBalls.Gameplay.Pawn
+{
+	public interface IDamageable
+	{
+		/// <returns>True if damage was applied.</returns>
+		bool TakeDamage( IDamageData data );
+	}
+}
