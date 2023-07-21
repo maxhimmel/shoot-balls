16c48c4c4d5571beb5725f855bd441c372073bbb
Max Himmel <himmelmax@gmail.com>
2023-07-21 15:48:11 -0700

    things can be damaged by their layer

---
+++ Assets/Scripts/Damageable/DamageDataByLayer.cs
@@ -0,0 +1,23 @@
+﻿using Sirenix.OdinInspector;
+using Sirenix.Utilities;
+using UnityEngine;
+
+namespace ShootBalls.Gameplay.Pawn
+{
+	[System.Serializable]
+	public class DamageDataByLayer
+	{
+		public LayerMask HitLayer;
+
+		[BoxGroup( "Damage (right-click to change type)" )]
+		[HideReferenceObjectPicker, LabelText( "@GetDamageTypeName()" )]
+		[SerializeReference] public IDamageData Damage;
+
+		private string GetDamageTypeName()
+		{
+			return Damage == null
+				? "Invalid"
+				: Damage.HandlerType.GetNiceName().SplitPascalCase();
+		}
+	}
+}
