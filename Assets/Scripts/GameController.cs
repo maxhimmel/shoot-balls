ceb0d3d23d3522be6406435b05504aaa7c353476
Max Himmel <himmelmax@gmail.com>
2023-07-28 13:39:49 -0700

    created spawn points for ball and player

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -11,22 +11,32 @@ namespace ShootBalls.Gameplay
 		private readonly PlayerController.Factory _playerFactory;
 		private readonly Ball.Factory _ballFactory;
 		private readonly Rewired.Player _playerInput;
+		private readonly Transform _ballSpawn;
+		private readonly Transform _playerSpawn;
 
 		public GameController( GameModel gameModel,
 			PlayerController.Factory playerFactory,
 			Ball.Factory ballFactory,
-			Rewired.Player playerInput )
+			Rewired.Player playerInput,
+
+			[Inject( Id = "Spawn_BallStart" )] Transform ballSpawn,
+			[Inject( Id = "Spawn_Player" )] Transform playerSpawn )
 		{
 			_gameModel = gameModel;
 			_playerFactory = playerFactory;
 			_ballFactory = ballFactory;
 			_playerInput = playerInput;
+			_ballSpawn = ballSpawn;
+			_playerSpawn = playerSpawn;
 		}
 
 		public void Initialize()
 		{
 			_gameModel.Player = _playerFactory.Create();
+			_gameModel.Player.Body.HolisticMove( _playerSpawn.position, _playerSpawn.rotation );
+
 			_gameModel.Ball = _ballFactory.Create();
+			_gameModel.Ball.Body.HolisticMove( _ballSpawn.position, _ballSpawn.rotation );
 
 			_gameModel.Player.Died += OnPlayerDied;
 		}
